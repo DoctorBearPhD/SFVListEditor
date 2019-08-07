68fbfd11509ddf497db35fa929f4e6b9ff303c4d
DoctorBearPhD <doctorbearphd@gmail.com>
2019-08-07 16:36:58 -0400

    Add project files.

---
+++ SFVAnimationsEditor/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace SFVAnimationsEditor.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
